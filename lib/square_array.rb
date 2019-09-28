256b8973d7a3a2dba2fbeece91517dc760bafe36
Nicole M Darby <nicoledarby021322@gmail.com>
2019-09-28 12:55:31 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,5 +6,10 @@ def square_array(array)
   new numbers=[9,10,16,25]
   
   square_array(new_numbers)
-  .while
 end
+    expect(numbers).to_not receive(:each)
+    expect(numbers).to_not receive(:collect)
+    expect(numbers).to_not receive(:map)
+    expect(numbers).to_not receive(:inject)
+    square_array(numbers)
+  end
